9264392fde7c0922dbfe766016d093760427a8d0
Brandon Zuech <brandonzuech@hotmail.com>
2019-12-08 23:05:00 -0500

    controller moved to shared

--- DashReportViewer.Shared/Controllers/ReportController.cs
+++ DashReportViewer.Shared/Controllers/ReportController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
-using DashReportViewer.Reports;
 using DashReportViewer.Shared.Models.Reporting;
 using DashReportViewer.Shared.Models.Widgets;
 using DashReportViewer.Shared.ReportComponents;
