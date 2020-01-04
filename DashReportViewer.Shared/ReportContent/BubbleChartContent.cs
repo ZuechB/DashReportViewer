286425544ed549158281d396e3bd4506cbdd2249
Brandon Zuech <Brandonzuech@hotmail.com>
2020-01-04 16:57:21 -0500

    bubble chart added

--- DashReportViewer.Shared/ReportContent/BubbleChartContent.cs
+++ DashReportViewer.Shared/ReportContent/BubbleChartContent.cs
@@ -4,17 +4,27 @@ namespace DashReportViewer.Shared.ReportContent
 {
     public class BubbleChartContent : BaseReportContent
     {
+        public BubbleHeader Header { get; set; }
         public List<BubbleDataPoint> dataPoints { get; set; }
         public string HorizontalText { get; set; }
         public string VerticalText { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class BubbleHeader
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
     }
 
     public class BubbleDataPoint
     {
         public string Id { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
         public string Name { get; set; } 
-        public int Size { get; set; }
+        public decimal Size { get; set; }
     }
 }
--- DashReportViewer/Reports/BubbleChartReport.cs
+++ DashReportViewer/Reports/BubbleChartReport.cs
@@ -6,12 +6,11 @@ using DashReportViewer.Shared.ReportContent;
 using DashReportViewer.Shared.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DashReportViewer.Reports
 {
-    [ReportName("Bubble Chart (Under Development)", "086A8665-C70D-4495-8469-4D462A505AC5", Description = "This is a test")]
+    [ReportName("Bubble Chart", "086A8665-C70D-4495-8469-4D462A505AC5", Description = "This is a test")]
     [ReportParams("Date", ReportInputType.DateRange, OrderId = 1)]
     public class BubbleChartReport : ReportEntity, IReport
     {
@@ -26,17 +25,76 @@ namespace DashReportViewer.Reports
                 var widgets = new List<Widget>();
 
 
+                var dataPoints = new List<BubbleDataPoint>();
+
+                dataPoints.Add(new BubbleDataPoint()
+                {
+                    Id = "USA",
+                    Name = "North America",
+                    X = 80.66m,
+                    Y = 1.67m,
+                    Size = 307007000.255m
+                });
+
+                dataPoints.Add(new BubbleDataPoint()
+                {
+                    Id = "Canada",
+                    Name = "North America",
+                    X = 60.66m,
+                    Y = 2.67m,
+                    Size = 307007000.255m
+                });
+
+                dataPoints.Add(new BubbleDataPoint()
+                {
+                    Id = "Italy",
+                    Name = "Europe",
+                    X = 20.66m,
+                    Y = 5.67m,
+                    Size = 307007200.255m
+                });
+
+
+
+
 
                 widgets.Add(new Widget("Sample Widget")
                 {
                     Content = new BubbleChartContent()
                     {
+                        Header = new BubbleHeader()
+                        {
+                            X = "Life Expectancy",
+                            Y = "Fertility Rate",
+                            Name = "Region",
+                            Size = "Population"
+                        },
+                        Title = "This is about the widget",
                         HorizontalText = "Horizontal Here",
                         VerticalText = "Vertical Here",
-                        dataPoints = null,
-                        //XAxis = new List<string>() { "Year", "2013", "2014", "2015", "2016" }
+                        dataPoints = dataPoints,
                     },
-                    Column = 12
+                    Column = 6
+                });
+
+
+                widgets.Add(new Widget("Sample Widget")
+                {
+                    Content = new BubbleChartContent()
+                    {
+                        Header = new BubbleHeader()
+                        {
+                            X = "Life Expectancy",
+                            Y = "Fertility Rate",
+                            Name = "Region",
+                            Size = "Population"
+                        },
+                        Title = "This is about the widget",
+                        HorizontalText = "Horizontal Here",
+                        VerticalText = "Vertical Here",
+                        dataPoints = dataPoints,
+                    },
+                    Column = 6
                 });
 
 
