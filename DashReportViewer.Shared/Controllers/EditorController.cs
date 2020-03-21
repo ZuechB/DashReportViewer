98c9aa8b34d87c9f09499413b5d17be3930979b5
Brandon Zuech <Brandonzuech@hotmail.com>
2020-03-20 20:42:37 -0400

    working on ace builder

---
+++ DashReportViewer.Shared/Controllers/EditorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashReportViewer.Shared.Controllers
+{
+    [Authorize]
+    public class EditorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Execute(string code)
+        {
+            // https://github.com/sebastienros/jint
+
+            return Json("");
+        }
+    }
+}
